86112c17e085d507403f44463166db7e1efeadce
Elijah Echekwu <64588466+Elijah57@users.noreply.github.com>
2024-02-23 09:41:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ dbConnect();
 
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(mongoSanitize());
 app.use(cors());
 app.use(rateLimiter)
