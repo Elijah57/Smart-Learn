c065e5dc035e6884063f2227703defdb6c176843
Elijah Echekwu <elijahechekwu30@gmail.com>
2024-03-10 17:35:54 +0100

    chore: Implement proper data validation on register input data

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -3,18 +3,30 @@ const asyncHandler = require('express-async-handler')
 const ejs = require("ejs");
 const path = require('path')
 const sendMail = require("../../utils/mails/mailer")
+const validateEmail = require("../../utils/helpers/validateEmail")
 require("dotenv").config()
 
 const HOST = process.env.HOST;
 
 const registerUser = asyncHandler ( async (req, res)=>{
     // Before user is registered check if email, has been created with another user before
-    const { email } = req.body;
+    const { firstname, lastname, email, password, confirmPassword } = req.body;
+
+    // perform input data checks
+    const isMailValid = validateEmail(email)
+    if (isMailValid === null) return res.status(400).json({status: false, message: "Invalid email address"})
+    
+    if ((typeof(password) !== "string") || (password === null) )return res.status(400).json({message: "Password invalid"});
+    if(password !== confirmPassword) return res.status(400).json({status: false, message: "Password does not match"})
+
+    const userInput = {firstname, lastname, email, password};
+
+    // check if email already exists
     const findUser = await User.findOne({email: email});
 
     // if user not found
     if(!findUser){
-        const createdUser = await User.create(req.body);
+        const createdUser = await User.create(userInput);
         const userVerificationCode = await createdUser.createActivationToken();
         const userActivationLink = `${HOST}/api/verify-email/${userVerificationCode}`
 
