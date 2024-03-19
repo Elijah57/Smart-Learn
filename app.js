76b62568750b152053017d87cb7167442804cdc9
Elijah Echekwu <elijahechekwu30@gmail.com>
2024-03-19 19:27:14 +0100

    chore: refactoring code

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const dotenv = require("dotenv").config();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./configs/swagger.json');
-const bodyParser = require('body-parser');
+const fileupload = require("express-fileupload");
 const cors = require('cors');
 const passport = require("passport");
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const mongoSanitize = require('express-mongo-sanitize');
+
 const dbConnect = require('./configs/dbConnect');
 const { notFound, handleError } = require('./middlewares/error/errorHandler');
 const googleRouter = require('./routers/googleRouter');
@@ -17,6 +18,7 @@ const courseLessonRouter = require('./routers/courseLessonRouter');
 const courseRouter = require('./routers/courseRouter');
 const rateLimiter = require("./middlewares/rateLimit")
 const passportSetup = require('./utils/passport/passportConfig');
+
 const studentRouter = require('./routers/studentRouter');
 
 const app = express();
@@ -25,13 +27,15 @@ app.set('trust proxy', false);
 const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST;
 
+const cloudinarySetup = require("./utils/cloudinary/cloudinarySetup")
+
 dbConnect();
 
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(mongoSanitize());
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(mongoSanitize());
+app.use(fileupload({useTempFiles: true}));
 // app.use(rateLimiter)
 
 app.use(session({
@@ -46,19 +50,17 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
-// passportSetup(passport);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {customSiteTitle: "SmartLearn API Docs"}));
+
 app.get("/", (req, res)=>{
-    res.send(`<a href="${HOST}/api-docs">Click Here to view API DOcumentation</a>`);
+    return res.send(`<a href="${HOST}/api-docs">Click Here to view API DOcumentation</a>`);
 });
 
 app.use('/', googleRouter);
 app.use("/api/auth", userRouter);
 app.use("/api/users", studentRouter);
 app.use("/api/admin", adminRouter);
-
-
 app.use("/api/courses", courseRouter);
 
 
