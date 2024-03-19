89fe04162ff167f28d9d968f3a77ed911b3b1928
Elijah Echekwu <elijahechekwu30@gmail.com>
2024-03-19 19:31:59 +0100

    feat: Password validation helper.js

--- utils/helpers/validatePassword.js
+++ utils/helpers/validatePassword.js
@@ -1,11 +1,11 @@
 const validatePassword = (password, res) => {
-    if (password === null) return res.status(400).json({message: "Please Provide a password"})
+    if (password === null) return res.status(400).json({message: "Please Provide a password"});
 
-    if (password.length < 6) return res.status(400).json({message: "Password should be at least 6 characters"})
+    if (password.length < 6) return res.status(400).json({message: "Password should be at least 6 characters"});
 
-    if (password.match(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/) === null) return res.status(400).json({message: "Password must contain at least one number, one lowercase and one uppercase letter"})
+    if (password.match(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/) === null) return res.status(400).json({message: "Password must contain at least one number, one lowercase and one uppercase letter"});
 
-    return String(password)
+    return String(password);
   };
 
 
