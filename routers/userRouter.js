2203803cf3ea8c705e252eaeeeee4f62619a2efa
Elijah Echekwu <elijahechekwu30@gmail.com>
2024-03-19 19:29:58 +0100

    chore: Added user profile update route

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { registerUser, loginUser, updateUser, verificationLink,
-    updatePassword, forgotPasswordToken, resetPassword, verifyUser} = require('../controllers/users/index');
+    updatePassword, forgotPasswordToken, resetPassword, verifyUser, updateProfileImage, getMyProfile} = require('../controllers/users/index');
 const { isLoggedIn } = require('../middlewares/auth/index');
 const userRouter = express.Router()
 
@@ -12,9 +12,11 @@ userRouter.post("/forgot-password", forgotPasswordToken);
 // get routes
 userRouter.get("/verify-email/:token", verifyUser);
 userRouter.get("/request-verification", isLoggedIn, verificationLink)
+userRouter.get("/profile-dashboard", isLoggedIn, getMyProfile);
 
 // put routes
 userRouter.put("/update-profile", isLoggedIn, updateUser);
+userRouter.put("/upload-profile-image", isLoggedIn, updateProfileImage);
 userRouter.put("/update-password", isLoggedIn, updatePassword)
 userRouter.put("/reset-password/:token", resetPassword)
 
