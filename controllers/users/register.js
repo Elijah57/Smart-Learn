2698822fca2d3b479eae8c0314c526d4973c65e5
Elijah Echekwu <elijahechekwu30@gmail.com>
2024-04-05 09:13:36 +0100

    chore: refactoring register.js

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -7,17 +7,15 @@ const validateEmail = require("../../utils/helpers/validateEmail")
 const validatePassword = require("../../utils/helpers/validatePassword")
 require("dotenv").config()
 
-const HOST = process.env.HOST;
+const HOST = process.env.HOST || "http://localhost/4000";
 
 const registerUser = asyncHandler ( async (req, res)=>{
-    // extract data from req.body
+    
     const { firstname, lastname, email, password, confirmPassword } = req.body;
 
-    // perform input data checks
-    const isMailValid = validateEmail(email)
-    if (isMailValid === null) {return res.status(400).json({status: false, message: "Invalid email address"})}
+    const isEmailValid = validateEmail(email)
+    if (isEmailValid === null) {return res.status(400).json({status: false, msg: "Invalid email address"})}
     
-
     const validatedPassword = validatePassword(password, res);
     if(validatedPassword !== confirmPassword){ return res.status(400).json({status: false, message: "Password does not match"}) }
 
@@ -27,35 +25,36 @@ const registerUser = asyncHandler ( async (req, res)=>{
     const findUser = await User.findOne({email: email});
 
     // if user not found
-    if(!findUser){
-        const createdUser = await User.create(userInput);
-        const userVerificationCode = await createdUser.createActivationToken();
-        const userActivationLink = `${HOST}/api/verify-email/${userVerificationCode}`
+    if(findUser){
+        return res.status(400).json({status: false, msg: "User with Email already Exists"})
+    }
+    const createdUser = await User.create(userInput);
+    const userVerificationCode = await createdUser.createActivationToken();
+    const userActivationLink = `${HOST}/api/verify-email/${userVerificationCode}`
 
-        const data= {user: createdUser.firstname, userActivationLink}
-        const html = await ejs.renderFile(path.join(__dirname, "../../mails/activation-mail.ejs"), data)
-    
-        try{
-            await sendMail({
-                email: createdUser.email,
-                subject: "Activate Your Account",
-                template: "activation-mail.ejs",
-                data
-            });
-        }catch(error){
-            return res.status(422).json({message: "Something went wrong!"})
-        }
-        res.status(200).json({
+    const emailData= {user: createdUser.firstname, userActivationLink};
+
+    try{
+        await sendMail({
+            email: createdUser.email,
+            subject: "Activate Your Account",
+            template: "activation-mail.ejs",
+            emailData
+        });
+
+        return res.status(200).json({
             status:true,
-            message: "User Created Successfully",
+            message: "User Created Successfully, check email for verification mail",
             createdUser
         })
-    }else{
-        return res.status(400).json({
-            status: false,
-            message: "User with Email already Exists"
-        })
+    }catch(error){
+        console.error("Error sending activation email:", error);
+        // await createdUser.remove(); // Rollback user creation
+        res.status(500).json({ status: false, message: "Failed to send activation email" });
+    
     }
-});
+        
+    }
+);
 
 module.exports = registerUser;
