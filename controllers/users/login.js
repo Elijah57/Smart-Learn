637d6a155d61cf44eb1c2f40911a6a7657080b7b
Elijah Echekwu <elijahechekwu30@gmail.com>
2024-04-11 17:15:15 +0100

    chore: refactoring login controller

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -4,34 +4,35 @@ const {generateToken} = require('../../utils/jwt/jwt');
 
 
 
-const loginUser = asyncHandler(async (req, res)=>{
-    const {email, password} = req.body;
-    if(email === null)return res.status(400).json({message: "Please provide your Email"});
-    if ((typeof(password) !== "string") || (password === null) )return res.status(400).json({message: "Password invalid"});
-    try{
-        const findUser = await User.findOne({email: email});
-        if(!findUser){
-            return res.status(400).json({status: false,
-                message:"Incorrect Email or Password"})
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json({ message: "Please provide both email and password" });
+    }
+
+    try {
+        const findUser = await User.findOne({ email: email });
+        if (!findUser) {
+            return res.status(400).json({ status: false, message: "Incorrect Email or Password" });
         }
 
-        if (findUser && (await findUser.isPasswordMatched(password))){
-            
-            res.status(200).json({
-                status:true,
+        const passwordMatched = await findUser.isPasswordMatched(password);
+
+        if (passwordMatched) {
+            return res.status(200).json({
+                status: true,
                 message: "Logged in Successfully",
-                token: generateToken(findUser?._id),
-                user_id: findUser?._id,
-                role: findUser?.roles,
-                username: findUser?.firstname +" "+ findUser?.lastname,
-                user_image: findUser?.user_image,     
-            })
-        }else{
-            return res.status(400).json({status: false,
-                message:"Incorrect Email or Password"})
+                token: generateToken(findUser._id),
+                user_id: findUser._id,
+                role: findUser.roles,
+                username: findUser.firstname + " " + findUser.lastname,
+            });
+        } else {
+            return res.status(400).json({ status: false, message: "Incorrect Email or Password" });
         }
-    }catch(error){
-        return;
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ status: false, message: "Internal Server Error" });
     }
 });
 
