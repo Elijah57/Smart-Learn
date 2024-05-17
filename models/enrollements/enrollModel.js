9c5d429bb040ba5e67c0b0a0ed302b16f55c7493
Elijah Echekwu <elijahechekwu30@gmail.com>
2024-05-17 05:38:56 +0100

    chore: modify enrollments

--- models/enrollements/enrollModel.js
+++ models/enrollements/enrollModel.js
@@ -3,8 +3,28 @@ const mongoose = require("mongoose")
 
 const EnrollmentSchema = mongoose.Schema({
 
-    
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    course_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course"
+    },
+    enrollment_date: {
+        type: Date
+    },
+    status: {
+        type: String,
+        enum: ["active", "completed", "withdrawn"]
+    },
+    progress: {
+        type: Number
+    }    
 }, 
 {
     timestamps: true
 })
+
+
+module.exports = mongoose.Model("Enrollment", EnrollmentSchema)
